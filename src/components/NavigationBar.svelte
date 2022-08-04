b1916f12b13b88e7f87c19f34d651e88579ceb49
Truong An Do <truongancpp8@gmail.com>
2022-08-04 23:20:41 +0700

    v0.01: Simple navigation bar added

---
+++ src/components/NavigationBar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+</script>
+<style lang="scss">
+    .navigationWrapper {
+        display: flex;
+        height: 64px;
+        flex-direction: row;
+        align-items: center;
+    }
+    .n_title {
+        font-size: x-large;
+        font-weight: 500;
+        margin-left: 16px;
+    }
+    .n_nav {
+        height: 24px;
+        font-size: x-large;
+        margin: 0 4px 0 auto;
+    }
+    .a_margin {
+        margin: 0 16px 0 16px;
+    }
+    .n_action {
+        transition: all 0.25s ease-in-out;
+    }
+    .n_action:hover {
+        color: #4A4A4A;
+    }
+</style>
+<div class="navigationWrapper">
+    <div class="n_title">
+        <p>DoAn08</p>
+    </div>
+    <div class="n_nav">
+        <a href="#!" class="material-symbols-rounded a_margin n_action">home</a>
+        <a href="#!" class="material-symbols-rounded a_margin n_action">help</a>
+    </div>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Nav from "../components/NavigationBar.svelte";
+</script>
+
+<Nav />
+
+<slot />
+
+<style lang="scss">
+</style>
